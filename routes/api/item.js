9fe41182f6cc73cf94f3d1aa5f82b41b4ccd8e34
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-19 17:41:20 -0300

    Create route to delete item

--- routes/api/item.js
+++ routes/api/item.js
@@ -25,4 +25,15 @@ router
     newItem.save().then(item => res.json(item));
   });
 
+router
+  .route("/:id")
+  // @route   POST api/items
+  // @desc    Create new item
+  // @access  Public
+  .delete((req, res) => {
+    Item.findById(req.params.id)
+      .then(item => item.remove().then(() => res.json({ success: true })))
+      .catch(err => res.status(404).json({ success: false }));
+  });
+
 module.exports = router;
