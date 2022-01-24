ff1b229fcef2bd497a5ef3ae94862a036cbec1cd
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-24 15:43:21 -0300

    Implement items actions and reducers with redux

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,22 +1,28 @@
-import { v4 as uuid } from "uuid";
-
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Eggs" },
-    { id: uuid(), name: "Milk" },
-    { id: uuid(), name: "Steak" },
-    { id: uuid(), name: "Water" },
-    { id: uuid(), name: "Candy" },
-  ],
+  items: [],
+  loading: false,
 };
 
 export default function itemReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return { ...state };
-
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload),
+      };
+    case ADD_ITEM:
+      return { ...state, items: [action.payload, ...state.items] };
+    case ITEMS_LOADING:
+      return { ...state, loading: true };
     default:
       return state;
   }
