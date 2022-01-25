a565c6b4c41ddc2d259e3e21e3afc142ad9315bb
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-25 20:42:16 -0300

    Create authentication route

---
+++ routes/api/auth.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+const User = require("../../models/User");
+
+router
+  .route("/")
+  // @route   POST api/auth
+  // @desc    Auth user
+  // @access  Public
+  .post((req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Please enter all fields" });
+    }
+
+    User.findOne({ email }).then(user => {
+      if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+      // Validate password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (!isMatch)
+          return res.status(400).json({ msg: "Invalid credentials" });
+
+        jwt.sign(
+          { id: user.id },
+          process.env.JWT_SECRET,
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              token,
+              user: { id: user.id, name: user.name, email: user.email },
+            });
+          }
+        );
+      });
+    });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose
 
 app.use("/api/items", require("./routes/api/items"));
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 // Serve static assets it in production
 if (process.env.NODE_ENV == "production") {
