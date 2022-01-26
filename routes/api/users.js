e865049809adc44783475089e1ca61129b9b0b0f
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-25 21:41:36 -0300

    Correct error with existing user

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,28 +20,28 @@ router
 
     User.findOne({ email }).then(user => {
       if (user) return res.status(400).json({ msg: "User already exists" });
-    });
 
-    const newUser = User({ name, email, password });
+      const newUser = User({ name, email, password });
 
-    // Create salt and hash
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser.save().then(user => {
-          jwt.sign(
-            { id: user.id },
-            process.env.JWT_SECRET,
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.json({
-                token,
-                user: { id: user.id, name: user.name, email: user.email },
-              });
-            }
-          );
+      // Create salt and hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save().then(user => {
+            jwt.sign(
+              { id: user.id },
+              process.env.JWT_SECRET,
+              { expiresIn: 3600 },
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  token,
+                  user: { id: user.id, name: user.name, email: user.email },
+                });
+              }
+            );
+          });
         });
       });
     });
